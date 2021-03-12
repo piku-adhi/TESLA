4a03497b6641051ad0a09a38044933c6092643c1
MrRobot222 <jokerhacker.6521@protonmail.com>
2021-03-12 14:01:21 +0530

    minor fix

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -7,7 +7,7 @@ from random import choice, randint
 
 from telethon.events import StopPropagation
 
-from userbot import (AFKREASON, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN)
+from userbot import (AFKREASON, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN, COUNT_MSG)
 from userbot.events import register
 
 AFKSTR = [
