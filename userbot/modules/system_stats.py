2db7a995fcd040fc50a048932295208509498119
MrRobot222 <jokerhacker.6521@protonmail.com>
2021-03-12 11:05:48 +0530

    update .alive

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -136,7 +136,7 @@ async def amireallyalive(alive):
               "==================== \n"
               f"`Telethon : v{version.__version__} `\n"
               f"`Python : v{python_version()} `\n"
-              f"'Version : {USERBOT_VERSION}'\n"
+              f"`Version : {USERBOT_VERSION}`\n"
               "==================== \n"
               f"`Running on Branch ==> {UPSTREAM_REPO_BRANCH} `\n"
               f"Tip:- Type `.help` to know about Modules Information \n")
